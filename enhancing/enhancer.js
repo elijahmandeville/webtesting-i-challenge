3c721fb12e6c73b90e56a43581a781a5af1157a0
elijahmandeville <elijahmandeville@gmail.com>
2020-05-12 20:28:50 -0600

    Finished MVP

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -1,22 +1,87 @@
-module.exports = {
-  succeed,
-  fail,
-  repair,
-  get,
-};
+let weapons = [
+  {
+    name: "Dawnblade",
+    durability: 95,
+    enhancement: 13,
+  },
+  {
+    name: "Sunshot",
+    durability: 46,
+    enhancement: 16,
+  },
+  {
+    name: "Bag of dirt",
+    durability: -1,
+    enhancement: 16,
+  },
+  {
+    name: "Chuck Norris",
+    durability: 100,
+    enhancement: 20,
+  },
+  {
+    name: "Garbage",
+    durability: "dirty socks",
+    enhancement: "it's garbage",
+  },
+];
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 0) {
+    item.enhancement = 0;
+  } else if (typeof item.enhancement !== "number") {
+    throw new Error("Item durability must be a number");
+  }
+
+  if (item.enhancement < 20) {
+    item.enhancement++;
+    return { ...item };
+  } else {
+    return { ...item };
+  }
 }
 
 function fail(item) {
+  //throws error if no number
+  if (typeof item.enhancement !== "number") {
+    throw new Error("Item enhancement must be a number");
+    //checks for enhancement greater than 15 ----------------------
+  } else if (item.enhancement >= 15) {
+    //checks for durability less than or equal to 0. keeps durability at 0
+    if (item.durability <= 0) {
+      return { ...item, durability: 0 };
+    }
+    //checks for enhancement being over 16, reduces it by 1 if so
+    if (item.enhancement >= 16) {
+      return {
+        ...item,
+        durability: item.durability - 10,
+        enhancement: item.enhancement - 1,
+      };
+    }
+    return { ...item, durability: item.durability - 10 };
+    //checks for less than 15 -------------------------------------
+  } else if (item.enhancement < 15) {
+    if (item.durability <= 0) {
+      return { ...item, durability: 0 };
+    }
+    return { ...item, durability: item.durability - 5 };
+  }
   return { ...item };
 }
 
 function repair(item) {
-  return { ...item };
+  return { ...item, durability: 100 };
 }
 
 function get(item) {
   return { ...item };
 }
+
+module.exports = {
+  succeed,
+  fail,
+  repair,
+  get,
+  weapons,
+};
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,70 @@
-const enhancer = require('./enhancer.js');
+const enhancer = require("./enhancer.js");
 // test away!
+
+describe("enhancer unit tests", () => {
+  beforeEach(() => {
+    enhancer.weapons = [
+      {
+        name: "Dawnblade",
+        durability: 95,
+        enhancement: 13,
+      },
+      {
+        name: "Sunshot",
+        durability: 46,
+        enhancement: 16,
+      },
+      {
+        name: "Bag of dirt",
+        durability: -1,
+        enhancement: 16,
+      },
+      {
+        name: "Chuck Norris",
+        durability: 100,
+        enhancement: 20,
+      },
+      {
+        name: "Garbage",
+        durability: "dirty socks",
+        enhancement: "it's garbage",
+      },
+    ];
+  });
+
+  it("repairs", () => {
+    expect(enhancer.repair(enhancer.weapons[0]).durability).toBe(100);
+    expect(enhancer.repair(enhancer.weapons[0]).durability).not.toBe(
+      enhancer.weapons[0].durability
+    );
+    expect(enhancer.repair(enhancer.weapons[1]).durability).toBe(100);
+    expect(enhancer.repair(enhancer.weapons[2]).durability).toBe(100);
+    expect(enhancer.repair(enhancer.weapons[3]).durability).toBe(100);
+  });
+
+  it("enhances", () => {
+    expect(enhancer.succeed(enhancer.weapons[0]).enhancement).toBe(
+      enhancer.weapons[0].enhancement++
+    );
+    expect(enhancer.succeed(enhancer.weapons[1]).enhancement).toBe(
+      enhancer.weapons[1].enhancement++
+    );
+    expect(enhancer.succeed(enhancer.weapons[2]).enhancement).toBe(
+      enhancer.weapons[2].enhancement++
+    );
+    expect(enhancer.succeed(enhancer.weapons[3]).enhancement).toBe(
+      enhancer.weapons[3].enhancement++
+    );
+    expect(() => enhancer.succeed(enhancer.weapons[4]).enhancement).toThrow();
+  });
+
+  it("enhancement fails", () => {
+    expect(enhancer.fail(enhancer.weapons[0]).durability).toBe(90);
+    expect(enhancer.fail(enhancer.weapons[1]).durability).toBe(36);
+    expect(enhancer.fail(enhancer.weapons[2]).durability).toBe(0);
+    expect(enhancer.fail(enhancer.weapons[3]).durability).toBe(90);
+    expect(enhancer.fail(enhancer.weapons[3]).enhancement).toBe(19);
+
+    expect(() => enhancer.fail(enhancer.weapons[4]).durability).toThrow();
+  });
+});
